cd59f3f2bc65fcbcf747391784d5bd26c127d299
Zbigniew Braniecki <gandalf@mozilla.com>
2012-01-21 15:40:04 +0100

    update tests

--- tests/html5/clientside/docdata/tests.js
+++ tests/html5/clientside/docdata/tests.js
@@ -1,9 +1,9 @@
 var utests = {
   'retranslate': function(nodes) {
     var button = document.getElementsByTagName('button')[0];
-    nodes.push(button);
     button.click();
     var mood = document.querySelectorAll('[l10n-id="mood"]')[0];
+    nodes.push(mood);
     assertEquals(mood.textContent, "He's happy!");
     mood.innerHTML = "She's happy!";
   },
