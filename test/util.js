678fd6dbbd789626c93a0a764fcccc675a43efea
Staś Małolepszy <stas@mozilla.com>
2019-12-16 13:09:39 +0100

    Remove the custom ftl dedent helper

--- test/util.js
+++ test/util.js
@@ -2,25 +2,6 @@ import fs from "fs";
 import color from "cli-color";
 import json_diff from "json-diff";
 
-function nonBlank(line) {
-    return !/^\s*$/.test(line);
-}
-
-function countIndent(line) {
-    const [indent] = line.match(/^\s*/);
-    return indent.length;
-}
-
-export function ftl(strings) {
-    const [code] = strings;
-    const lines = code.split("\n").slice(1, -1);
-    const indents = lines.filter(nonBlank).map(countIndent);
-    const common = Math.min(...indents);
-    const indent = new RegExp(`^\\s{${common}}`);
-    const dedented = lines.map(line => line.replace(indent, ""));
-    return `${dedented.join("\n")}\n`;
-}
-
 export function readdir(path) {
     return new Promise(function(resolve, reject) {
         fs.readdir(path, function(err, filenames) {
